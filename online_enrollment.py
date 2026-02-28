e212877dfca6ad7d7a083cb00bed5ddcdacbb1bf
Noan Cliffe <153498848+NoanCliffe@users.noreply.github.com>
2026-02-28 21:33:24 +0800

    Add files via upload

---
+++ online_enrollment.py
@@ -0,0 +1,212 @@
+import os
+import random
+from PIL import Image
+import io
+import base64
+
+from xjtulogin import XJTULogin
+import requests
+from urllib.parse import urlparse, parse_qs
+
+def get_aosp_builds(ver):
+    if ver == 13:
+        return random.choice(["TP1A", "TQ2A", "TKQ1","TKP1","TKP2"])
+    elif ver == 14:
+        return random.choice(["UP1A", "UQ1A","UP1B", "UQ1A","UK2A", "UK1B"])
+    elif ver == 15:
+        bstr=''
+        bstr+=random.choice(['A','B','C'])
+        bstr+=random.choice(['P','Q','K'])
+        bstr+=random.choice(['1','2','3'])
+        bstr+=random.choice(['A','B'])
+        return bstr
+chrome_versions = [
+    "121.0.6167.139",
+    "121.0.6167.160",
+    "121.0.6167.184",
+    "121.0.6167.85",
+    "122.0.6261.111",
+    "122.0.6261.128",
+    "122.0.6261.57",
+    "122.0.6261.69",
+    "122.0.6261.94",
+    "123.0.6312.105",
+    "123.0.6312.122",
+    "123.0.6312.58",
+    "123.0.6312.86",
+    "124.0.6367.118",
+    "124.0.6367.155",
+    "124.0.6367.201",
+    "124.0.6367.207",
+    "124.0.6367.60",
+    "124.0.6367.78",
+    "124.0.6367.91",
+    "125.0.6422.112",
+    "125.0.6422.141",
+    "125.0.6422.60",
+    "125.0.6422.76",
+    "126.0.6478.114",
+    "126.0.6478.126",
+    "126.0.6478.182",
+    "126.0.6478.55",
+    "126.0.6478.61",
+    "127.0.6533.119",
+    "127.0.6533.72",
+    "127.0.6533.88",
+    "127.0.6533.99",
+    "128.0.6613.113",
+    "128.0.6613.119",
+    "128.0.6613.137",
+    "128.0.6613.84",
+    "129.0.6668.100",
+    "129.0.6668.58",
+    "129.0.6668.70",
+    "129.0.6668.89",
+    "130.0.6723.116",
+    "130.0.6723.58",
+    "130.0.6723.69",
+    "130.0.6723.91",
+    "131.0.6778.108",
+    "131.0.6778.139",
+    "131.0.6778.204",
+    "131.0.6778.264",
+    "131.0.6778.69",
+    "131.0.6778.85",
+    "132.0.6834.110",
+    "132.0.6834.159",
+    "132.0.6834.83",
+    "133.0.6943.126",
+    "133.0.6943.141",
+    "133.0.6943.53",
+    "133.0.6943.98",
+    "134.0.6998.117",
+    "134.0.6998.165",
+    "134.0.6998.35",
+    "134.0.6998.88",
+    "135.0.7049.114",
+    "135.0.7049.52",
+    "135.0.7049.84",
+    "135.0.7049.95",
+    "136.0.7103.113",
+    "136.0.7103.59",
+    "136.0.7103.92",
+    "137.0.7151.103",
+    "137.0.7151.119",
+    "137.0.7151.55",
+    "137.0.7151.68",
+    "138.0.7204.100",
+    "138.0.7204.157",
+    "138.0.7204.168",
+    "138.0.7204.183",
+    "138.0.7204.49",
+    "138.0.7204.92",
+    "139.0.7258.127",
+    "139.0.7258.138",
+    "139.0.7258.154",
+    "139.0.7258.66",
+    "140.0.7339.127",
+    "140.0.7339.185",
+    "140.0.7339.207",
+    "140.0.7339.80",
+    "141.0.7390.107",
+    "141.0.7390.122",
+    "141.0.7390.54",
+    "141.0.7390.65",
+    "141.0.7390.76",
+    "142.0.7444.134",
+    "142.0.7444.162",
+    "142.0.7444.175",
+    "142.0.7444.59",
+    "143.0.7499.109",
+    "143.0.7499.146",
+    "143.0.7499.169",
+    "143.0.7499.192",
+    "143.0.7499.40",
+    "144.0.7559.109",
+    "144.0.7559.132",
+    "144.0.7559.59",
+    "144.0.7559.96",
+    "145.0.7632.109",
+    "145.0.7632.116",
+    "145.0.7632.45",
+    "145.0.7632.67",
+    "145.0.7632.75"
+]
+hello_entrance = "https://org.xjtu.edu.cn/openplatform/oauth/authorize?responseType=code&scope=user_info&appId=755&state=1234&redirectUri=http%3A%2F%2Fhello.xjtu.edu.cn%2Fyingxin%2Flogin%2Fxjtu%2Foauth%2Fapp"
+
+Android_version=random.randint(13, 15)
+
+model_list=['2211133C','2304FPN6DC','23054RA19C','24101PNB7C','2509FPN0BC','25069PTEBG','25098PN5AC','25113PN0EG','I2508','V2546A','V2538','V2511','V2514','V2516A','PMC110','CPH2801','CPH2789','CPH2813','CPH2825','CPH2791','CPH2763','CPH2757']
+sysBuild={13:[]}
+address_list=['西安交通大学(创新港校区)','涵英楼','西安交通大学创新港校区足球场','西安交通大学空天与动力研究院','西安交通大学创新港校区学生宿舍惠园','创新港-西安交通大学自动化科学与工程研究分院']
+
+client=requests.session()
+client.headers['X-Requested-With']='com.supwisdom.xjtu'
+
+client.headers['systemType']='yingxin_student_app'
+client.headers['User-Agent']='Mozilla/5.0 (Linux; Android {}; {} Build/{}.{:0>2}{:0>2}{:0>2}.{:0>3}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{} Mobile Safari/537.36 SuperApp'.format(Android_version, random.choice(model_list), get_aosp_builds(Android_version), random.randint(24,25), random.randint(1, 12), random.randint(1, 28), random.randint(0, 99),random.choice(chrome_versions))
+latitude='{:.6f}'.format(random.uniform(34.25, 34.26))
+longitude='{:.6f}'.format(random.uniform(108.65, 108.67))
+try:
+    netid=input("输入你的NetID:")
+    pswd=input("输入你的密码:")
+    login_handle = XJTULogin(netid, pswd)
+    redirect_url=login_handle.login(hello_entrance)
+except XJTULogin.LoginError as e:
+    print("登录失败:", str(e))
+    input("按回车键退出...")
+    exit(1)
+except Exception as e:
+    print("发生未知错误:", str(e))
+    input("按回车键退出...")
+    exit(1)
+
+print("登录成功，重定向URL:", redirect_url)
+final_response = client.get(redirect_url, allow_redirects=False)
+if final_response.status_code == 302:
+    final_url=final_response.headers['Location']
+    parsed_url = urlparse(final_url)
+    query_params = parse_qs(parsed_url.query)
+    token=query_params.get('info', [None])[0]
+    if not token:
+        print("重定向URL中未找到token参数")
+        input("按回车键退出...")
+        exit(1)
+    print("重定向成功，用户身份key:", token[:15]+"...")
+    img_path=None
+    image_data=None
+    while not img_path:
+        img_path=input("输入你的人脸照片路径或直接将照片文件拖放进来，然后按回车键继续:")
+        if not os.path.exists(img_path):
+            print("文件路径不存在，请重新输入。")
+            img_path=None
+        if not os.path.isfile(img_path):
+            print("输入的路径不是一个文件，请重新输入。")
+            img_path=None
+        if img_path and os.path.isfile(img_path):
+            try:
+                img = Image.open(img_path)
+                buffered = io.BytesIO()
+                img.save(buffered, format="PNG")
+                encoded_data = base64.b64encode(buffered.getvalue()).decode("utf-8")
+                image_data = f"data:image/png;base64,{encoded_data}"
+            except Exception as e:
+                print("无法打开或处理图片文件，请确保文件是有效的图片格式。错误详情:", str(e))
+                print("请重新输入图片路径。")
+                img_path=None
+    client.headers['access_token']=token
+    req_headers = {
+        'Referer':'http://hello.xjtu.edu.cn/yingxin-student/photo',
+        'Origin':'http://hello.xjtu.edu.cn'
+    }
+    payload={
+        'flowId':2223,
+        'imageFile':image_data,
+        "latitude": latitude,
+        "longitude": longitude,
+        "address": random.choice(address_list),
+        'synAccessSource':'h5'
+    }
+    response=client.post("http://hello.xjtu.edu.cn/yingxin/checkIn/saveOrUpdate",json=payload,headers=req_headers)
+    print("签到响应:", response.text)
+    input("按回车键退出...")
---
+++ xjtulogin.py
@@ -0,0 +1,233 @@
+from hashlib import md5
+import requests
+import time
+import logging
+from bs4 import BeautifulSoup
+
+class XJTULogin:
+    
+    class LoginError(Exception):
+        def __init__(self, *args: object) -> None:
+            super().__init__(*args)
+
+    def __init__(self,username:str|int,pswd:str,encrypted=False,debug=False) -> None:
+        '''创建登录句柄
+        Parameters:
+            username:任何用于统一认证的用户名
+            pswd:统一认证平台密码，默认情况下为明文
+            encrypted:密码是否已加密，加密遵循RSA-PKCS#1 v1.5方式，密钥为https://login.xjtu.edu.cn/cas/jwt/publicKey
+            debug:是否打印调试信息
+        '''
+        self.logger=logging
+        self.debug=debug
+        if debug:
+            logging.basicConfig(level=logging.DEBUG)
+        self.webClient=requests.session()
+        self.webClient.headers['User-Agent']='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+        self.username=str(username)
+        self.fingerprint=str(md5(f"{username}{pswd}@XJTULogin".encode()).hexdigest())
+        if not encrypted:
+            import base64
+            try:
+                from Crypto.PublicKey import RSA
+                from Crypto.Cipher import PKCS1_v1_5
+            except ModuleNotFoundError as e:
+                raise Exception('Crypto module not found, please run "pip install pycryptodome" or encrypt your password manually and set encrypted=True')
+            try:
+                res=self.webClient.get('https://login.xjtu.edu.cn/cas/jwt/publicKey')
+                if res.status_code != 200:
+                    self.logger.warning('Failed to fetch public key for encryption, status code: %s, using cached public key...', res.status_code)
+                    pub_key=RSA.import_key('-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2u2v/bjSIVsaxCBBxkjWf7LpmsjuhFJUJE7MYTn9hBcDXlK4smgtNoMqmGz4ztg5t1h+h0fqrJT3WkdoLV/FKC8OwElTe+p+YLqA6/PgmGtsffcQmAW0eye5NygiWM+B0tO69ML6jNLpAWAvXwod5kr/k7qsM1DGTux+e7bjdFz/IA8vOZx3IlGHnX+RE/uBJUwPXHnLPw5pQSwkWwfpPwxMrgzwik6htqRHF2c7Z+pJToXbrIJWD5nmRiU6jzgu8ncLqbMb3WNOKSodcEnlUpTH/ApH56IOJHWpq3mxJL9DaUaWzjziR93wjlyvR1K4VM7TLqD35CVZQaoE5FWgZwIDAQAB\n-----END PUBLIC KEY-----')
+                else:
+                    pub_key=RSA.import_key(res.text)
+            except requests.RequestException as e:
+                self.logger.warning('Failed to fetch public key for encryption: %s, using cached public key...', e)
+                pub_key=RSA.import_key('-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2u2v/bjSIVsaxCBBxkjWf7LpmsjuhFJUJE7MYTn9hBcDXlK4smgtNoMqmGz4ztg5t1h+h0fqrJT3WkdoLV/FKC8OwElTe+p+YLqA6/PgmGtsffcQmAW0eye5NygiWM+B0tO69ML6jNLpAWAvXwod5kr/k7qsM1DGTux+e7bjdFz/IA8vOZx3IlGHnX+RE/uBJUwPXHnLPw5pQSwkWwfpPwxMrgzwik6htqRHF2c7Z+pJToXbrIJWD5nmRiU6jzgu8ncLqbMb3WNOKSodcEnlUpTH/ApH56IOJHWpq3mxJL9DaUaWzjziR93wjlyvR1K4VM7TLqD35CVZQaoE5FWgZwIDAQAB\n-----END PUBLIC KEY-----')
+            handle = PKCS1_v1_5.new(pub_key)
+            pwen = handle.encrypt(pswd.encode())
+            self.pswdEncryptd = '__RSA__' + base64.b64encode(pwen).decode()
+        else:
+            self.pswdEncryptd=pswd
+
+    def __check_mfa_phone(self,code):
+        self.logger.debug('Checking MFA phone from code: %s', code)
+        parmas={'state1':code}
+        res=self.webClient.get('https://login.xjtu.edu.cn/cas/mfa/initByType/securephone',params=parmas)
+        self.logger.debug('MFA phone check response: %s', res.text)
+        if res.status_code!=200:
+            self.logger.debug('MFA phone check http error, response code: %s', res.status_code)
+            raise self.LoginError('MFA phone check failed, code {}'.format(res.status_code))
+        if res.json()['code']!=0:
+            self.logger.debug('MFA phone check api error, response raw: %s', res.text)
+            raise self.LoginError('MFA phone check failed, {}'.format(res.json()['data']))
+        return res.json()['data']
+
+    def __send_mfa_sms(self,gid):
+        self.logger.debug('Sending MFA SMS to gid: %s', gid)
+        res=self.webClient.post('https://login.xjtu.edu.cn/attest/api/guard/securephone/send',json={'gid':gid})
+        self.logger.debug('MFA SMS send response: %s', res.text)
+        if res.status_code!=200:
+            self.logger.debug('MFA SMS send http error, response code: %s', res.status_code)
+            raise self.LoginError('MFA SMS send failed, code {}'.format(res.status_code))
+        if res.json()['code']!=0:
+            self.logger.debug('MFA SMS send api error, response raw: %s', res.text)
+            raise self.LoginError('MFA SMS send failed, {}'.format(res.json()['message']))
+        
+    def __verfy_mfa_sms(self,sms_code,gid):
+        if not sms_code or sms_code=='':
+            self.logger.debug('No SMS code provided, will resend SMS.')
+            print('检测到空验证码，等待重新发送短信验证码...')
+            for i in range(60,0,-1):
+                print(f'\r重新发送倒计时: {i} 秒',end='\r',flush=True)
+                time.sleep(1)
+            print('\n')
+            return False,False
+        self.logger.debug('Verifying MFA SMS with id: %s and sms_code: %s', gid, sms_code)
+        res=self.webClient.post('https://login.xjtu.edu.cn/attest/api/guard/securephone/valid',json={'code':sms_code,'gid':gid})
+        self.logger.debug('MFA SMS verify response: %s', res.text)
+        if res.status_code!=200:
+            self.logger.debug('MFA SMS verify http error, response code: %s', res.status_code)
+            raise self.LoginError('MFA SMS verify failed, code {}'.format(res.status_code))
+        if res.json()['code']!=0:
+            self.logger.debug('MFA SMS verify api error, response raw: %s', res.text)
+            raise self.LoginError('MFA SMS verify failed, {}'.format(res.json()['data']['message']))
+        if res.json()['data']['status'] == 3:
+            self.logger.debug('MFA SMS verify failed, invalid SMS code.')
+            print('验证码错误，请重新输入。')
+            return True,True
+        elif res.json()['data']['status'] == 2:
+            self.logger.debug('MFA SMS verify successed.')
+            print('验证成功')
+            return True,False
+        else:
+            self.logger.debug('MFA SMS verify failed, unknown status: %s', res.json()['data']['status'])
+            raise self.LoginError('MFA SMS verify failed, unknown status {}'.format(res.json()['data']['status']))
+
+    def login(self,orgURL,identity=None,consistent_fingerprint=None):
+        '''登录
+        Parameters:
+            orgURL:原平台的登陆地址，类似于"https://login.xjtu.edu.cn/cas/login?service=..."(新版原生)或"http://org.xjtu.edu.cn/openplatform/oauth/authorize?appId=..."(旧版org兼容跳转)
+            identity:该统一认证账户有多个身份情况下，指定身份(学工号)***暂未实现***
+            consistent_fingerprint:如果需要保持一致的浏览器追踪指纹，请提供一个一致的指纹字符串(小写32位十六进制字符串)，否则会根据学号密码自动生成一个新的指纹
+        Returns:
+            str:为统一认证平台返回的重定向url，请根据平台的登录逻辑处理
+        '''
+        if identity is not None:
+            self.logger.error('Identity parameter is not implemented yet, if you have multiple identities, please report it to the developer to help implement this feature.')
+            raise NotImplementedError('Identity parameter is not implemented yet')
+        if orgURL.startswith('http://'):
+            orgURL = orgURL.replace('http://','https://',1)
+        if not (orgURL.startswith('https://login.xjtu.edu.cn') or orgURL.startswith('https://org.xjtu.edu.cn')):
+            self.logger.error('Invalid orgURL, must start with "https://login.xjtu.edu.cn" or "https://org.xjtu.edu.cn",see Parameters for details.')
+            raise ValueError('Invalid orgURL')
+        res=self.webClient.get(orgURL)
+        call_back_url=res.url
+        self.logger.debug('login callback URL: %s', call_back_url)
+        page = BeautifulSoup(res.text, 'html.parser')
+        forms=page.find_all("form")
+        login_data={}
+        for form in forms:
+            if form.get('id')!='fm1':
+                continue
+            for tag in form.find_all('input'):
+                login_data[tag.get('name')]=tag.get('value',default='')
+                if tag.get('v-model')=='passwordLoginUsername':
+                    login_data[tag.get('name')]=self.username
+                elif tag.get('v-model')=='passwordLoginPassword':
+                    login_data[tag.get('name')]=self.pswdEncryptd
+            login_data['fpVisitorId']=consistent_fingerprint or self.fingerprint
+            login_data.pop('rememberMe', None) #不删报错
+        self.logger.debug('login data:{}'.format(login_data))
+        self.logger.debug('Checking MFA status...')
+        res=self.webClient.post('https://login.xjtu.edu.cn/cas/mfa/detect',data={'username':self.username,'password':self.pswdEncryptd,'fpVisitorId':login_data['fpVisitorId']})
+        self.logger.debug('MFA status response:{}'.format(res.text))
+        code=res.json()['data']['state']
+        self.logger.debug('MFA status code:{}'.format(code))
+        mfa_successed=not res.json()['data']['need']
+        while not mfa_successed:
+            if not res.json()['data']['mfaTypeSecurePhone']:
+                raise NotImplementedError('MFA type not supported yet, you must enable phone sms MFA. If you have enabled phone sms MFA and still see this error, please report it to the developer.')
+            #raise self.LoginError('MFA is required, please report it to the developer.')
+            print('!!!MFA is required, please complete MFA verification first.')
+            self.logger.debug('getting MFA state sms code...')
+            phone_info=self.__check_mfa_phone(code)
+            phone_number=phone_info['securePhone']
+            print(f'SMS code will be sent to your registered phone number: {phone_number}')
+            self.__send_mfa_sms(phone_info['gid'])
+            sms_code_still_valid=True
+            while sms_code_still_valid:
+                sms_code = input('输入收到的短信验证码，留空重新获取: ').strip()
+                mfa_successed,sms_code_still_valid = self.__verfy_mfa_sms(code, sms_code, phone_info['gid'])
+
+
+        login_data['mfaState']=code
+        res=self.webClient.post(call_back_url,data=login_data,allow_redirects=False)
+        if res.status_code==401:
+            raise self.LoginError('Login failed, please check your username and password')
+        elif res.status_code==200:
+            raise self.LoginError('Unknown error, please report it to the developer.')
+        while res.status_code==302 and ('org.xjtu.edu.cn' in res.headers['Location'] or 'login.xjtu.edu.cn' in res.headers['Location']):
+            res=self.webClient.get(res.headers['Location'],allow_redirects=False)
+        return res.headers.get('Location', '')
+
+    def login_old_org(self,orgURL,identity=None):
+        '''登录旧版org平台(鬼知道能不能用，鬼知道有没有用)
+        Parameters:
+            orgURL:原平台的登陆地址
+            identity:该统一认证账户有多个身份情况下，指定身份(学工号)
+        Returns:
+            str:为统一认证平台返回的重定向url，请根据平台的登录逻辑处理
+        '''
+        res = self.webClient.get(orgURL)
+        logindata={"loginType":1,"username":self.username,"pwd":self.pswdEncryptd,"jcaptchaCode":""}
+        res=self.webClient.post('https://org.xjtu.edu.cn/openplatform/g/admin/login',json=logindata)
+        self.logger.debug('Step 1:login','request',username=self.username)
+        if res.status_code!=200:
+            self.logger.debug('Step 1:login','http error',response_code=res.status_code)
+            raise self.LoginError('login failed, code {}'.format(res.status_code))
+        if res.json()['code']!=0:
+            self.logger.debug('Step 1:login','api error',response_raw=res.text)
+            raise self.LoginError('login failed, {}'.format(res.json()['message']))
+        self.orgToken = res.json()['data']['tokenKey']
+        self.orgMemberID = res.json()['data']['orgInfo']['memberId']
+        self.orgState = res.json()['data']['state']
+        self.logger.debug('Step 1:login','response',orgToken=self.orgToken,orgMemberID=self.orgMemberID,state=self.orgState,raw=res.json())
+        self.webClient.cookies['memberId']=str(self.orgMemberID)
+        self.webClient.cookies['open_Platform_User']=self.orgToken
+        self.webClient.cookies['state']=self.orgState
+        res = self.webClient.post('https://org.xjtu.edu.cn/openplatform/g/admin/getUserIdentity',data={'memberId':self.orgMemberID})
+        if res.status_code!=200:
+            self.logger.debug('Step 2: get user id','http error',response_code=res.status_code)
+            raise self.LoginError('failed to get user Identity, code{}'.format(res.status_code))
+        elif res.json()['code']!=0:
+            self.logger.debug('Step 2: get user id','api error',response_raw=res.text)
+            raise self.LoginError('failed to get user Identity, {}'.format(res.json()['message']))
+        identitys=res.json()['data']
+        self.logger.debug('Step 2: get user id','response',identitys=identitys,response_raw=res.json())
+        if len(identitys)>1:
+            
+            ChosenIdentity=None
+            if not identity:
+                identity=self.username
+            self.logger.debug('Step 3: choose identity','requests',identitys=len(identitys),given_identity=identity)
+            for id in identitys:
+                if id['personNo']==str(identity):
+                    ChosenIdentity=id
+                    break
+            if not ChosenIdentity:
+                self.logger.debug('Step 3: choose identity','error',identitys=identitys,given_identity=identity)
+                raise self.LoginError('no match indentity found. Please use student ID to login or specify identity')
+        else:
+            ChosenIdentity=identitys[0]
+            self.logger.debug('Step 3: choose identity','response',ChosenIdentity=ChosenIdentity)
+        redirparam={"userType":ChosenIdentity['userType'],'personNo':ChosenIdentity['personNo'],"_":str(int(time.time()*1000))}
+        res = self.webClient.get('https://org.xjtu.edu.cn/openplatform/oauth/auth/getRedirectUrl',params=redirparam)
+        if res.status_code!=200:
+            self.logger.debug('Step 4: get redirect URL','http error',response_code=res.status_code)
+            raise self.LoginError('Failed to get redirect URL, code {}'.format(res.status_code))
+        if res.json()['code']!=0:
+            self.logger.debug('Step 4: get redirect URL','api error',response_raw=res.text)
+            raise self.LoginError('Failed to get redirect URL, {}'.format(res.json()['message']))
+        redirURL=res.json()['data']
+        self.logger.debug('Step 4: get redirect URL','response',redirect_URL=redirURL,response_raw=res.json())
+        return redirURL
